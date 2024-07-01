97cf672e557d1e7f5fafd7f5d5e5e5286313aa00
이재하 <jhl2619@naver.com>
2024-07-02 00:54:32 +0900

    feat: Implement search page with search input and result table components

--- new-web/src/app/page.tsx
+++ new-web/src/app/page.tsx
@@ -1,38 +1,20 @@
-import Link from "next/link";
+import { Header } from "components";
 
-import { buttonVariants } from "components/button";
+import { SearchInput, SearchResultTable } from "./search/components";
 
-export default function IndexPage() {
+export default function SearchPage() {
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
-        </p>
-      </div>
-      <div className="flex gap-4">
-        <Link
-          href="/"
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href="/"
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
-      </div>
-    </section>
+    <>
+      <Header>Companies</Header>
+      <main className="flex flex-col items-center w-full px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-6xl mt-8">
+          <h2 className="text-2xl font-semibold text-gray-800 mb-6">
+            Search for a Company
+          </h2>
+          <SearchInput />
+          <SearchResultTable />
+        </div>
+      </main>
+    </>
   );
 }
