58c0f5d8f77d880a708c352444d7fb683dd11a05
이재하 <jhl2619@naver.com>
2024-07-08 01:39:24 +0900

    feat: Add lazy loading for search container component

--- new-web/src/app/page.tsx
+++ new-web/src/app/page.tsx
@@ -1,12 +1,15 @@
 import { Header } from "components";
 import { SearchContainer } from "./search/components";
+import { Suspense } from "react";
 
 const SearchPage = () => {
   return (
     <>
       <Header>Companies</Header>
       <main className="flex flex-col items-center w-full px-4 sm:px-6 lg:px-8">
-        <SearchContainer />
+        <Suspense fallback={<></>}>
+          <SearchContainer />
+        </Suspense>
       </main>
     </>
   );
