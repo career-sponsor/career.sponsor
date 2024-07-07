f71457ad04b4a31568e064b2be8d63fa706b9bdc
이재하 <jhl2619@naver.com>
2024-07-08 01:22:50 +0900

    feat: Add link to homepage in header component

--- new-web/src/components/header.tsx
+++ new-web/src/components/header.tsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import { PropsWithChildren } from "react";
 
 function Header({ children }: PropsWithChildren) {
   return (
     <header className=" w-full py-4 border-b border-gray-100">
       <div className="container mx-auto px-6 text-2xl font-bold text-gray-800">
-        {children}
+        <Link href="/">{children}</Link>
       </div>
     </header>
   );
