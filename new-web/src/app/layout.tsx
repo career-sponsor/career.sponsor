13e5c3888f024dc249cbf20bbb2290fa4c01bd9d
이재하 <jhl2619@naver.com>
2024-07-14 17:32:20 +0900

    chore: Add Google Tag Manager

--- new-web/src/app/layout.tsx
+++ new-web/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "lib/styles/globals.css";
+import { GoogleTagManager } from "@next/third-parties/google";
+import { GTM_ID } from "lib/env";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId={GTM_ID} />
       <body className={inter.className}>{children}</body>
     </html>
   );
