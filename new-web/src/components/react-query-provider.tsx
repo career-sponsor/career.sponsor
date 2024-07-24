9fada78f30ea6395bc650cb29c42f9a616ca4fdb
이재하 <jhl2619@naver.com>
2024-07-24 18:47:03 +0900

    feat: Refactor ReactQueryProvider to use cached query client

--- new-web/src/components/react-query-provider.tsx
+++ new-web/src/components/react-query-provider.tsx
@@ -1,26 +1,18 @@
 "use client";
 
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren } from "react";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { getQueryClient } from "lib";
 
 export const ReactQueryProvider = ({ children }: PropsWithChildren) => {
-  const [reactQueryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            refetchIntervalInBackground: false,
-          },
-        },
-      })
-  );
+  const queryClient = getQueryClient();
 
   return (
-    <QueryClientProvider client={reactQueryClient}>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
