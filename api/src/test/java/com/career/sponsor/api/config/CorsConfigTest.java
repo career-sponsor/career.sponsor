2f4a424fd6cb31ebf57b1487399c20798f4bc3a9
Peter Park <peter.park@zalando.de>
2024-10-09 17:56:18 +0200

    add cors

---
+++ api/src/test/java/com/career/sponsor/api/config/CorsConfigTest.java
@@ -0,0 +1,28 @@
+package com.career.sponsor.api.config;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CorsConfigTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void testCorsHeader() throws Exception {
+        mvc.perform(get("/api/v1/companies/search")
+                .header("Origin", "http://localhost:8080"))
+                .andExpect(status().isOk());
+
+    }
+}
